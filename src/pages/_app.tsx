3ba741263cce652ab214a1d63a628c283731d812
Kevin Beltrao de Melo <kbmelo@outlook.com>
2023-09-28 16:09:42 -0300

    only render after mount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app';
 import GlobalStyle, { theme } from '@/styles/GlobalStyle';
+import { useEffect, useState } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // https://stackoverflow.com/questions/75094010/nextjs-13-hydration-failed-because-the-initial-ui-does-not-match-what-was-render
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <></>;
+
   return (
     <>
       <GlobalStyle />
